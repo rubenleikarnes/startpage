1bf1ca1a022ce204cac8b7bbba1358bd60756fc4
rub1 <ruben.leikarnes@gmail.com>
2015-10-16 21:59:35 +0200

    Moved to gulp from rake, removed double named images

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # My personal chrome startpage
 
 I've never liked the default chrome startpage (new tab), so i created my own. You can find it on the [Chrome webstore](https://chrome.google.com/webstore/detail/new-tab-speeddial/iaicolincinfdmmdnokcnedfibkjpjld).
+
+Dev
+
+```
+gulp serve
+```
+
+Build
+
+```
+gulp build
+```
---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp'),
+	del = require('del'),
+	inlinesource = require('gulp-inline-source'),
+	compass = require('gulp-compass'),
+	browserSync = require('browser-sync'),
+	exec = require('child_process').exec;
+
+
+gulp.task('clean', function () {
+	return del([
+		'./dist/*'
+	])
+})
+
+gulp.task('compass:watch', function() {
+	exec('compass watch');
+});
+
+gulp.task('compass:build', ['clean'], function() {
+	exec('compass compile -e production --force');
+});
+
+gulp.task('inlinesource', ['compass:build'], function () {
+    return gulp.src('./app/*.html')
+        .pipe(inlinesource())
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy', function () {
+	return gulp.src([
+		'app/img/*.png', 
+		'app/manifest.json'
+		], {
+            base: 'app'
+        }).pipe(gulp.dest('dist'));
+})
+
+gulp.task('build', ['inlinesource'], function() {
+    gulp.start('copy');
+});
+
+gulp.task('serve', ['compass:watch'], function() {
+	browserSync.init({
+		server: "app",
+		index: "main.html"
+	});
+
+	gulp.watch('app/css/**/*.css').on('change', browserSync.reload);
+	gulp.watch("app/*.html").on('change', browserSync.reload);
+	gulp.watch('app/img/**/*').on('change', browserSync.reload);
+});
